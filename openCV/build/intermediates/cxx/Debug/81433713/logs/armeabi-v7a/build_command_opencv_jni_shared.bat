e295678ca7d27ece42c07d8db9badb5d8a30440e
dozopravka <dozopravka@example.com>
2023-03-17 23:52:37 +0300

    camera

---
+++ openCV/build/intermediates/cxx/Debug/81433713/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\YVZ\\CV\\openCV\\.cxx\\Debug\\81433713\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/81433713/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\YVZ\\CV\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=30" ^
+  "-DANDROID_PLATFORM=android-30" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\vipev\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\YVZ\\CV\\openCV\\build\\intermediates\\cxx\\Debug\\81433713\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\YVZ\\CV\\openCV\\build\\intermediates\\cxx\\Debug\\81433713\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\YVZ\\CV\\openCV\\.cxx\\Debug\\81433713\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
