c5da07cd6eb31f800a9e4ea69de7b7492957b6fc
dozopravka <dozopravka@example.com>
2023-03-18 11:14:24 +0300

    blur

--- app/src/main/java/com/example/cv/MainActivity.java
+++ app/src/main/java/com/example/cv/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cv;
 
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +14,27 @@ import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends CameraActivity  {
 
     CameraBridgeViewBase cameraBridgeViewBase;
+    CascadeClassifier cascadeClassifier;
+    Mat gray, rgb, transpose_gray, transpose_rgb;
+    MatOfRect rects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +48,71 @@ public class MainActivity extends CameraActivity  {
         cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
             @Override
             public void onCameraViewStarted(int width, int height) {
-
+                rgb = new Mat();
+                gray = new Mat();
+                rects = new MatOfRect();
             }
 
             @Override
             public void onCameraViewStopped() {
-
+                rgb.release();
+                gray.release();
+                rects.release();
             }
 
             @Override
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-                return inputFrame.rgba();
+                rgb = inputFrame.rgba();
+                gray = inputFrame.gray();
+
+                transpose_gray = gray.t();
+                transpose_rgb = rgb.t();
+
+                MatOfRect rects = new MatOfRect();
+
+                cascadeClassifier.detectMultiScale(transpose_gray, rects, 1.08, 2);
+
+                for(Rect rect: rects.toList()){
+
+                    Mat submat = transpose_rgb.submat(rect);
+
+                    Imgproc.blur(submat, submat, new Size(20, 20));
+                    Imgproc.rectangle(transpose_rgb, rect, new Scalar(0, 255, 0), 10);
+
+
+                  }
+                return transpose_rgb.t();
             }
         });
         
         if(OpenCVLoader.initDebug()){
             cameraBridgeViewBase.enableView();
+
+            try{
+                InputStream inputStream = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                File file = new File(getDir("cascade", MODE_PRIVATE), "lbpcascade_frontalface.xml");
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+                byte[] data = new byte[4096];
+                int read_bytes;
+
+                while ((read_bytes = inputStream.read(data)) != -1){
+                    fileOutputStream.write(data, 0, read_bytes);
+                }
+
+
+                cascadeClassifier = new CascadeClassifier(file.getAbsolutePath());
+                if(cascadeClassifier.empty()) cascadeClassifier = null;
+
+                inputStream.close();
+                fileOutputStream.close();
+                file.delete();
+
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
